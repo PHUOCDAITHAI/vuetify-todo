0dbfbec2e1c6079abd08ab89d1525a356a7b0d13
phuoc dai <daithai050900@gmail.com>
2022-06-06 22:03:24 +0700

    Datepickers

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -22,6 +22,28 @@
             <v-form class="px-3">
                 <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
                 <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+                <v-menu
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                >
+                    <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                        v-model="due"
+                        label="Due date"
+                        prepend-icon="mdi-calendar"
+                        v-bind="attrs"
+                        v-on="on"
+                    ></v-text-field>
+                    </template>
+                    <v-date-picker
+                    v-model="due"
+                    @input="menu2 = false"
+                    ></v-date-picker>
+                </v-menu>
                 <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
             </v-form>
         </v-card-text>
@@ -35,7 +57,10 @@ export default {
     data(){
         return {
             title: '',
-            content: ''
+            content: '',
+            due: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+            menu2: false,
+            modal: false,
         }
     },
     methods: {
