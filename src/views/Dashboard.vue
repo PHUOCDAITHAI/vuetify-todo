b773801198254b2585fc58d2391c0a3e223eb68f
phuoc dai <daithai050900@gmail.com>
2022-06-06 17:33:35 +0700

    Sorting Projects

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,16 @@
   <div class="dashboard">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <v-btn small outlined color="grey" @click="sortBy('title')" class="btn-folder">
+          <v-icon left small >mdi-folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn small outlined color="grey" @click="sortBy('person')" class="btn-folder">
+          <v-icon left small >mdi-account</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
+      </v-layout>
       <v-card class="white pa-3 mb-1" v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -39,6 +49,11 @@
           { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         ]
       }
+    },
+    methods: {
+      sortBy(prop){
+        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+      }
     }
   }
 </script>
@@ -60,5 +75,8 @@
 }
 .v-chip.overdue{
   background: #f83e70 !important;
+}
+.btn-folder {
+  border: none;
 }
 </style>
