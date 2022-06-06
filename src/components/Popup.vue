d826bf1c791b4d11a9b233ed6d7d39b5687f0319
phuoc dai <daithai050900@gmail.com>
2022-06-06 21:31:34 +0700

    Form-Basics

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -3,20 +3,28 @@
     <v-dialog
       width="600"
     >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          class="success"
-          v-bind="attrs"
-          v-on="on"
-        >
-          Add new project
-        </v-btn>
-      </template>
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn
+            class="success"
+            v-bind="attrs"
+            v-on="on"
+            >
+            Add new project
+            </v-btn>
+        </template>
 
-      <v-card>
+        <v-card>
         <v-card-title>
-          <h2>Add a New Project</h2>
+            <h2>Add a New Project</h2>
         </v-card-title>
+
+        <v-card-text>
+            <v-form class="px-3">
+                <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+                <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+                <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+            </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </div>
@@ -24,10 +32,16 @@
 
 <script>
 export default {
-
+    data(){
+        return {
+            title: '',
+            content: ''
+        }
+    },
+    methods: {
+        submit(){
+            console.log(this.title, this.content);
+        }
+    }
 }
 </script>
-
-<style>
-
-</style>
